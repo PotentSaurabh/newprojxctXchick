59b8170483b8034755f6ea430e240afd731b86ad
Abhay Pratap Singh <80690695+Cool69-69@users.noreply.github.com>
2022-10-31 00:50:40 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -44,7 +44,7 @@ async def batch(client: Client, message: Message):
 async def link_generator(client: Client, message: Message):
     while True:
         try:
-            pehla_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..\nor Send 480P Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            pehla_message = await client.ask(text = "Send 480P Post File", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         first_msg_id = await get_message_id(client, pehla_message)
@@ -57,7 +57,7 @@ async def link_generator(client: Client, message: Message):
             
     while True:
         try:
-            dusra_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..\nor Send 720P HEVC Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            dusra_message = await client.ask(text = "Send 720P HEVC File", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         second_msg_id = await get_message_id(client, dusra_message)
@@ -69,7 +69,7 @@ async def link_generator(client: Client, message: Message):
      
     while True:
         try:
-            tesra_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..\nor Send 720P X264 Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            tesra_message = await client.ask(text = "Send 720P X264 File", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         third_msg_id = await get_message_id(client, tesra_message)
@@ -81,7 +81,7 @@ async def link_generator(client: Client, message: Message):
     
     while True:
         try:
-            chautha_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..\nor Send 1080P X264 Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            chautha_message = await client.ask(text = "Send 1080P X264 File", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         fourth_msg_id = await get_message_id(client, chautha_message)
