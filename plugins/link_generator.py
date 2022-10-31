7c6155eea221903a55089c8e501d6d753a4dfde0
Abhay Pratap Singh <80690695+Cool69-69@users.noreply.github.com>
2022-10-31 12:24:49 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -91,7 +91,7 @@ async def link_generator(client: Client, message: Message):
             break
         else:
             await chautha_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
-            fourth_file_size = async get_size(chautha_message)
+            fourth_file_size = async def get_size(chautha_message)
             continue
 
     first_base64_string = await encode(f"get-{first_msg_id * abs(client.db_channel.id)}")
