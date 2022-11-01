9d504ce893882bf2af275d401e77e32224a868d0
Abhay Pratap Singh <80690695+Cool69-69@users.noreply.github.com>
2022-11-01 13:26:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -22,14 +22,3 @@ async def get_shortlink(link):
             else:
                 return f"Error: {data['message']}"
             
-async def get_size(size):
-    """Get size in readable format"""
-
-    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
-    size = float(size)
-    i = 0
-    while size >= 1024.0 and i < len(units):
-        i += 1
-        size /= 1024.0
-    return "%.2f %s" % (size, units[i])
-
