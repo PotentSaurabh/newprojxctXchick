2676a426dcffc666c84266985c279417e9eda8df
Abhay Pratap Singh <80690695+Cool69-69@users.noreply.github.com>
2022-11-03 00:45:58 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message
 from bot import Bot
 from config import ADMINS
 from helper_func import encode, get_message_id, get_message_mg
-from utils import get_shortlink
+from utils import get_shortlink, get_size
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
@@ -123,4 +123,5 @@ async def link_generator(client: Client, message: Message):
     third_link = await get_shortlink(f"https://telegram.me/{client.username}?start={third_base64_string}")
     fourth_base64_string = await encode(f"get-{fourth_msg_id * abs(client.db_channel.id)}")
     fourth_link = await get_shortlink(f"https://telegram.me/{client.username}?start={fourth_base64_string}")
-    await chatha_message.reply_text(f"<a href='{sixth_msg_id}'>🎬</a> 𝐓𝐢𝐭𝐥𝐞: <b>{fifth_msg_id}</b>\n🔊 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞: <b>English & Hindi</b>\n🎞 𝐐𝐮𝐚𝐥𝐢𝐭𝐲: <b>WEBRip</b>\n<b>〰〰〰〰〰〰〰〰〰〰〰\n🧑‍💻How to Download :\nWatch </b>👉\n<b>https://t.me/HeavenForYouAll/7878</b>\n<b>〰〰〰〰〰〰〰〰〰〰〰\n\n480p x264 []\n👉{first_link}\n\n720p x265 []\n👉{second_link}\n\n720p x264 []\n👉{third_link}\n\n1080p x264 []\n👉{fourth_link}\n\n.........................................................\n🎯 Join : </b><b>@HeavenForYouAll</b>\n<b>🎯 Join : </b><b>@HeavenRequest</b>\n<b>---------------------------------------------\nTo get Latest Movies/Series faster with Ad-free experience, get your Premium membership through </b><b>@HeavenPremiumBot</b><b>.\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°</b>", quote=True)
+    first_size = await get_size(pehla_message.media)
+    await chatha_message.reply_text(f"<a href='{sixth_msg_id}'>🎬</a> 𝐓𝐢𝐭𝐥𝐞: <b>{fifth_msg_id}</b>\n🔊 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞: <b>English & Hindi</b>\n🎞 𝐐𝐮𝐚𝐥𝐢𝐭𝐲: <b>WEBRip</b>\n<b>〰〰〰〰〰〰〰〰〰〰〰\n🧑‍💻How to Download :\nWatch </b>👉\n<b>https://t.me/HeavenForYouAll/7878</b>\n<b>〰〰〰〰〰〰〰〰〰〰〰\n\n480p x264 [{first_size}]\n👉{first_link}\n\n720p x265 []\n👉{second_link}\n\n720p x264 []\n👉{third_link}\n\n1080p x264 []\n👉{fourth_link}\n\n.........................................................\n🎯 Join : </b><b>@HeavenForYouAll</b>\n<b>🎯 Join : </b><b>@HeavenRequest</b>\n<b>---------------------------------------------\nTo get Latest Movies/Series faster with Ad-free experience, get your Premium membership through </b><b>@HeavenPremiumBot</b><b>.\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°</b>", quote=True)
