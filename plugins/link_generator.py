cf9313911be10723f554fb0162a126b4f2e07f96
Abhay Pratap Singh <80690695+Cool69-69@users.noreply.github.com>
2022-11-03 23:01:57 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -9,11 +9,99 @@ from helper_func import encode, get_message_id, get_message_mg
 from utils import get_shortlink
 from temp import get_size
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('series'))
 async def batch(client: Client, message: Message):
     while True:
         try:
-            first_message = await client.ask(text = "Forward the First Message from DB Channel (with Quotes)..\n\nor Send the DB Channel Post Link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            ek_message = await client.ask(text = "Forward the First 480P Ep. Post from Database channel WITH Tag", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+        except:
+            return
+        ek_msg_id = await get_message_id(client, ek_message)
+        if ek_msg_id:
+            break
+        else:
+            await ek_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
+            continue
+
+    while True:
+        try:
+            do_message = await client.ask(text = "Forward the Last 480P Ep. Post from Database channel WITH Tag", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+        except:
+            return
+        do_msg_id = await get_message_id(client, do_message)
+        if do_msg_id:
+            break
+        else:
+            await do_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
+            continue
+    
+    while True:
+        try:
+            teen_message = await client.ask(text = "Forward the First 720P Ep. Post from Database channel WITH Tag", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+        except:
+            return
+        teen_msg_id = await get_message_id(client, teen_message)
+        if teen_msg_id:
+            break
+        else:
+            await teen_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
+            continue
+    
+    while True:
+        try:
+            char_message = await client.ask(text = "Forward the Last 720P Ep. Post from Database channel WITH Tag", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+        except:
+            return
+        char_msg_id = await get_message_id(client, char_message)
+        if char_msg_id:
+            break
+        else:
+            await char_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
+            continue
+    
+    while True:
+        try:
+            panch_message = await client.ask(text = "Forward the Series NAME with other details from Database channel WITH Tag", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+        except:
+            return
+        panch_msg_id = await get_message_mg(client, panch_message)
+        if panch_msg_id:
+            break
+        else:
+            await panch_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
+            continue
+    
+    while True:
+        try:
+            che_message = await client.ask(text = "Forward the Series POSTER from Database channel WITH Tag", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+        except:
+            return
+        che_msg_id = await get_message_mg(client, che_message)
+        if che_msg_id:
+            break
+        else:
+            await che_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
+            continue
+
+
+
+    ek_string = f"get-{ek_msg_id * abs(client.db_channel.id)}-{do_msg_id * abs(client.db_channel.id)}"
+    ek_base64_string = await encode(ek_string)
+    ek_link =  await get_shortlink(f"https://telegram.me/{client.username}?start={ek_base64_string}")
+    do_string = f"get-{teen_msg_id * abs(client.db_channel.id)}-{char_msg_id * abs(client.db_channel.id)}"
+    do_base64_string = await encode(do_string)
+    do_link =  await get_shortlink(f"https://telegram.me/{client.username}?start={base64_string}")
+    media_ek = ek_message.video
+    ek_size = get_size(media_ek.file_size)
+    media_do = teen_message.video
+    do_size = get_size(media_do.file_size)
+    await che_message.reply_text(f"<a href='{che_msg_id}'>🎬</a> 𝐓𝐢𝐭𝐥𝐞: <b>{panch_msg_id}</b>\n🔊 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞: <b>English & Hindi</b>\n🎞 𝐐𝐮𝐚𝐥𝐢𝐭𝐲: <b>WEBRip</b>\n📧 𝐒𝐮𝐛𝐭𝐢𝐭𝐥𝐞𝐬: <b>Esubs</b>\n<b>〰〰〰〰〰〰〰〰〰〰〰\n🧑‍💻How to Download :\nWatch </b>👉\n<b>https://t.me/HeavenForYouAll/7878</b>\n<b>〰〰〰〰〰〰〰〰〰〰〰\n\n480p x264 [Around {ek_size} per Episode]\n👉{ek_link}\n\n720p x264 [Around {do_size} per Episode]\n👉{do_link}\n\n.........................................................\n🎯 Join : </b><b>@HeavenForYouAll</b>\n<b>🎯 Join : </b><b>@HeavenRequest</b>\n<b>---------------------------------------------\nTo get Latest Movies/Series faster with Ad-free experience, get your Premium membership through </b><b>@HeavenPremiumBot</b><b>.\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°</b>", quote=True)
+ 
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('single'))
+async def batch(client: Client, message: Message):
+    while True:
+        try:
+            first_message = await client.ask(text = "Forward the First Ep. Post from Database channel WITH Tag", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         f_msg_id = await get_message_id(client, first_message)
@@ -25,7 +113,7 @@ async def batch(client: Client, message: Message):
 
     while True:
         try:
-            second_message = await client.ask(text = "Forward the Last Message from DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            second_message = await client.ask(text = "Forward the Last Ep. Post from Database channel WITH Tag", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         s_msg_id = await get_message_id(client, second_message)
@@ -42,11 +130,12 @@ async def batch(client: Client, message: Message):
     await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True)
 
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
+
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('movie'))
 async def link_generator(client: Client, message: Message):
     while True:
         try:
-            pehla_message = await client.ask(text = "Send 480P Post File", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            pehla_message = await client.ask(text = "Forward 480P File Post from Database channel WITH Tag", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         first_msg_id = await get_message_id(client, pehla_message)
@@ -59,7 +148,7 @@ async def link_generator(client: Client, message: Message):
             
     while True:
         try:
-            dusra_message = await client.ask(text = "Send 720P HEVC File", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            dusra_message = await client.ask(text = "Forward 720P HEVC File Post from Database channel WITH Tag", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         second_msg_id = await get_message_id(client, dusra_message)
@@ -71,7 +160,7 @@ async def link_generator(client: Client, message: Message):
      
     while True:
         try:
-            tesra_message = await client.ask(text = "Send 720P X264 File", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            tesra_message = await client.ask(text = "Forward 720P File Post from Database channel WITH Tag", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         third_msg_id = await get_message_id(client, tesra_message)
@@ -83,7 +172,7 @@ async def link_generator(client: Client, message: Message):
     
     while True:
         try:
-            chautha_message = await client.ask(text = "Send 1080P X264 File", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            chautha_message = await client.ask(text = "Forward 1080P File Post from Database channel WITH Tag", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         fourth_msg_id = await get_message_id(client, chautha_message)
@@ -95,7 +184,7 @@ async def link_generator(client: Client, message: Message):
             
     while True:
         try:
-            panchma_message = await client.ask(text = "Send Movie name", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            panchma_message = await client.ask(text = "Forward Movie Name Post from Database channel WITH Tage", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         fifth_msg_id = await get_message_mg(client, panchma_message)
@@ -107,7 +196,7 @@ async def link_generator(client: Client, message: Message):
     
     while True:
         try:
-            chatha_message = await client.ask(text = "Send Poster Link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            chatha_message = await client.ask(text = "Forward POSTER Link Post from Database channel WITH Tag", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         sixth_msg_id = await get_message_mg(client, chatha_message)
