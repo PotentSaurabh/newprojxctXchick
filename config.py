d28d7f09df0cee86ae1cd40aeee5b60d4d7ae160
Abhay Pratap Singh <80690695+Cool69-69@users.noreply.github.com>
2022-12-31 10:24:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,8 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://target:target@cluster0.8qkog2s.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "heavenx")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
